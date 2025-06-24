f583eeb6752c820f105a96ba6eae23a39481a15c
Shashanka <poudelshashanka205@gmail.com>
2025-06-24 13:15:14 +0545

    latest changes

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,3 +1,11 @@
-const BASE_URL = "http://localhost:4000/api"; // Update this for production
+// const BASE_URL = "http://localhost:4000/api"; // Update this for production
+
+// export default BASE_URL;
+
+
+const BASE_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:4000/api"
+        : "https://gre-vocab-backend.onrender.com";
 
 export default BASE_URL;
